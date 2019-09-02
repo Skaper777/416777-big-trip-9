7c90d05dea2adbbc6ff398fca455dbef378d09d8
Skaper777 <skaper777@gmail.com>
2019-09-02 17:06:27 +0300

    flatpickr

--- src/components/points.js
+++ src/components/points.js
@@ -4,4 +4,4 @@ import {
 
 const EVENT_COUNT = 3;
 
-export const events = new Array(EVENT_COUNT).fill(``).map(getEvent).sort((a, b) => a.time.timeIn - b.time.timeIn);
+export const events = new Array(EVENT_COUNT).fill(``).map(getEvent).sort((a, b) => b.time.timeIn - a.time.timeIn);
